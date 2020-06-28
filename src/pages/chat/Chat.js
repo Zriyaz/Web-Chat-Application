f11cda5631f3ee27008c6c8d2fbe33a1400f59af
Md Riyaz Kazmi <mdriyazuddine17@gmail.com>
2020-06-28 18:53:38 +0000

    commit

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -1,6 +1,8 @@
 import React from "react"
 import LoginString from "../login/loginStrings"
 import firebase from "../../services/firebase"
+import "./Chat.css"
+import ReactLoading from 'react-loading'
 
 export default class Chat extends React.Component{
 	constructor(props){
@@ -12,7 +14,6 @@ export default class Chat extends React.Component{
 		this.props.history.push('/')
 		localStorage.clear()
 	}
-
 	render(){
 		return(
 			<div>User : {this.currentUserName}
